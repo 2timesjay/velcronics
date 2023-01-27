84994d74d8dc267a7cfa47abe5fb699027068c02
unknown <2timesjay@gmail.com>
2023-01-27 09:08:24 -0500

    react stub

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 // vite.config.js
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   build: {
@@ -10,8 +11,10 @@ export default defineConfig({
         books: resolve(__dirname, 'books/index.html'),
         personal: resolve(__dirname, 'personal/index.html'),
         processing: resolve(__dirname, 'projects/processing/index.html'),
-        botorch: resolve(__dirname, 'projects/botorch/index.html')
+        botorch: resolve(__dirname, 'projects/botorch/index.html'),
+        spw: resolve(__dirname, 'projects/spw/index.html'),
       }
     }
-  }
-})
+  },
+  plugins: [react()],
+});
