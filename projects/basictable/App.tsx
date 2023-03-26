376bf3ffba95f5af661bb3dd5b988fb5ae1ab725
unknown <2timesjay@gmail.com>
2023-03-25 23:09:59 -0400

    more concise version

--- projects/basictable/App.tsx
+++ projects/basictable/App.tsx
@@ -1,76 +1,102 @@
 import React, { useMemo, useState } from 'react';
 import DataGrid from 'react-data-grid';
-import DeltaCellFormatter from './DeltaCellFormatter';
 import 'react-data-grid/lib/styles.css';
 
-const SummaryRow = ({ id, totalCount, deltaSum }) => {
-  return <>{`${Math.floor((100 * deltaSum) / totalCount)}%`}</>;
-};
+interface SummaryRow {
+  id: string;
+  totalCount: number;
+  deltaSum: number;
+}
 
 const deltaValues = [-1, 0, 1];
 
-const columns = [
-  { key: 'id', name: 'ID' },
-  { key: 'title', name: 'Title' },
-  {
-    key: 'delta',
-    name: 'Delta',
-    editable: true,
-    width: 80,
-    formatter: ({ row, onRowChange }) => {
-      var deltaValuesIndex = deltaValues.findIndex((v) => v === row.delta);
-      return (
-        <DeltaCellFormatter
-          value={row.delta}
-          onChange={() => {
-            const newDeltaValuesIndex = (deltaValuesIndex + 1) % deltaValues.length;
-            console.log("new row", { ...row, delta: deltaValues[newDeltaValuesIndex] })
-            onRowChange({ ...row, delta: deltaValues[newDeltaValuesIndex] });
-          }}
-        />
-      );
-    },
-    summaryFormatter: ({ row }) => {
-      return (
-        <SummaryRow
-          id={row.id}
-          totalCount={row.totalCount}
-          deltaSum={row.deltaSum}
-        />
-      );
-    },
-  },
-];
-
-const rows = [
-  { id: 0, title: 'Example', delta: -1 },
-  { id: 1, title: 'Demo', delta: 1 },
-  { id: 2, title: 'Illustration', delta: 0 },
-];
-
-function App() {
-  const [rowsState, setRowsState] = useState(rows);
-
-  const summaryRows = useMemo(() => {
-    const summaryRow = {
-      id: 'total_0',
-      totalCount: rowsState.length,
-      deltaSum: rowsState.reduce((partialSum, { delta }) => partialSum + delta, 0),
-    };
-    return [summaryRow];
-  }, [rowsState]);
+function DeltaCellFormatter({ value, onChange }: { value: number; onChange: () => void }) {
+  let backgroundColor = '';
+  switch (value) {
+    case -1:
+      backgroundColor = '#ffc7ce';
+      break;
+    case 1:
+      backgroundColor = '#c6efce';
+      break;
+    default:
+      backgroundColor = '#fff';
+      break;
+  }
 
   return (
-    <div className="App">
-      <DataGrid
-        columns={columns}
-        rows={rowsState}
-        onRowsChange={setRowsState}
-        topSummaryRows={summaryRows}
-        bottomSummaryRows={summaryRows}
-      />
+    <div 
+      onClick={onChange} 
+      style={{
+        backgroundColor,
+        padding: '5px',
+        borderRadius: '5px',
+        textAlign: 'center',
+        cursor: 'pointer',
+      }}>
+      {value}
     </div>
   );
 }
 
-export default App;
+const columns = [
+  { key: "id", name: "ID" },
+  { key: "title", name: "Title" },
+  {
+    key: "delta",
+    name: "Delta",
+    width: 80,
+    formatter({ row, onRowChange }: GridCellProps) {
+      const deltaValueIndex = deltaValues.indexOf(row.delta);
+
+      return (
+        <DeltaCellFormatter
+          value={row.delta}
+          onChange={() => {
+            onRowChange({
+              ...row,
+              delta: deltaValues[(deltaValueIndex + 1) % deltaValues.length],
+            });
+          }}
+          />
+        );
+      },
+      summaryFormatter({ row: { deltaSum, totalCount } }: { row: SummaryRow }) {
+        return <>{`${Math.floor((100 * deltaSum) / totalCount)}%`}</>;
+      },
+    },
+  ];
+  
+  const initialRows = [
+    { id: 0, title: "Example", delta: -1 },
+    { id: 1, title: "Demo", delta: 1 },
+    { id: 2, title: "Illustration", delta: 0 },
+  ];
+  
+  function App() {
+    const [rows, setRows] = useState(initialRows);
+  
+    const summaryRows = useMemo(() => {
+      const summaryRow: SummaryRow = {
+        id: "total_0",
+        totalCount: rows.length,
+        deltaSum: rows.reduce((partialSum, a) => partialSum + a.delta, 0),
+      };
+      return [summaryRow];
+    }, [rows]);
+  
+    return (
+      <div className="App">
+        <DataGrid
+          columns={columns}
+          rows={rows}
+          onRowsChange={setRows}
+          topSummaryRows={summaryRows}
+          bottomSummaryRows={summaryRows}
+        />
+      </div>
+    );
+  }
+  
+  export default App;
+  
