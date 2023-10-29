b4aaaea58bd1a21f3d8ca94cb58fd3dff562279f
unknown <2timesjay@gmail.com>
2023-10-29 00:17:44 -0400

    remove random scripts

--- src/reviews.ts
+++
@@ -1,5 +0,0 @@
-export function setupReviews(element: HTMLElement) {
-  element.addEventListener('click', () => console.log("You clicked on a review."))
-}
-  
-setupReviews(document.querySelector<HTMLElement>('#reviews')!)
