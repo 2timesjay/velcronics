c5c2148b48327845e7087a40d0d26ae334110ac3
unknown <2timesjay@gmail.com>
2023-10-29 00:04:37 -0400

    simplify vite config

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/routes/index.html'),
-        books: resolve(__dirname, 'src/routes/books/index.html'),
-        personal: resolve(__dirname, 'src/routes/personal/index.html'),
-        visualizations: resolve(__dirname, 'src/routes/projects/visualizations/index.html'),
-        botorch: resolve(__dirname, 'src/routes/projects/botorch/index.html'),
-        sidebyside: resolve(__dirname, 'src/routes/projects/sidebyside/index.html'),
-        basictable: resolve(__dirname, 'src/routes/projects/basictable/index.html'),
       }
     }
   },
